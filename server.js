76e402b8c86f3066d2718b1dfb7c565f3ad4b0bc
Matt Leuer <mattleuer@gmail.com>
2020-04-23 17:04:56 -0700

    Post and Delete routes working

--- server.js
+++ server.js
@@ -9,14 +9,6 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-async function dbAsArray() {
-    let data = [];
-    fs.readFile(path.join(__dirname, "db", "db.json"), "utf-8", (err, data) => {
-        if (err) throw err;
-        console.log(data);
-        return JSON.parse(data);
-    });
-}
 
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public", "notes.html"));
@@ -31,15 +23,49 @@ app.get("/api/notes", function (req, res) {
 
 app.post("/api/notes", function (req, res) {
     const note = req.body;
-    let dbArr = dbAsArray();
-    dbArr.push(note);
 
-    fs.writeFile(path.join(__dirname, "db", "db.json"), JSON.stringify(dbArr), (err) => {
-        if(err) throw err;
-        Console.log("Database updated");
-    })
+    fs.readFile(path.join(__dirname, "db", "db.json"), "utf-8", (err, data) => {
+        if (err) throw err;
+
+        let dbArray = [];
+        if (data) {
+            dbArray = JSON.parse(data);
+        }
+
+        note.id = dbArray.length + 1;
+        dbArray.push(note);
+
+        fs.writeFile(path.join(__dirname, "db", "db.json"), JSON.stringify(dbArray), (err) => {
+            if (err) throw err;
+            Console.log("Database updated");
+        })
+    });
     res.send(note);
 });
+
+app.delete("/api/notes/:id", function (req, res) {
+    const id = parseInt(req.params.id);
+
+    fs.readFile(path.join(__dirname, "db", "db.json"), "utf-8", (err, data) => {
+        if (err) throw err;
+
+        let dbArray = [];
+        if (data) {
+            dbArray = JSON.parse(data);
+        }
+        dbArray = dbArray.filter(element => {
+            return element.id !== id;
+        });
+        console.log(dbArray);
+
+        fs.writeFile(path.join(__dirname, "db", "db.json"), JSON.stringify(dbArray), (err) => {
+            if (err) throw err;
+            console.log("Database updated");
+            res.sendStatus(200);
+        })
+    });
+});
+
 
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "public", "index.html"));
@@ -50,5 +76,3 @@ app.get("*", function (req, res) {
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
-
-dbAsArray();
